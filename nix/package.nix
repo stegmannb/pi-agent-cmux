06c9eaf341763c93c836e2efe4e4e6b46a0d94e1
Bastian Stegmann <bastian.stegmann@bearingpoint.com>
2026-05-22 09:40:10 +0200

    fix(nix): correct fetchPnpmDeps hash after upstream rebase

--- nix/package.nix
+++ nix/package.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 3;
-    hash = "sha256-sRmHUMgQ9TAsbvEKeZw5wXtJ9sZnoz9ahnTQ3hMRdg0=";
+    hash = "sha256-97m0wmoXOWWTCR6CN9E0k597lZkEYc+xlraPh0f/JDo=";
   };
 
   nativeBuildInputs = [
